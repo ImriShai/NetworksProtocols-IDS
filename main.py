a2067add8c778aa93248b3c83568498dbb7eeab7
hagaycohen2 <hagaycohen2@gmail.com>
2024-09-24 18:45:40 +0300

    Co-authored-by: Imri Shai <ImriShai@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from threading import Thread, Event
 import argparse
 import time
 from colorama import Fore, Style, init
+import pandas as pd
 
 
 # TODO: general explanation of the script and implementation details
@@ -134,21 +135,25 @@ def packet_callback(packet):
     global flow_statistics, AUTHORIZED_IPS, alerts
 
     if packet.haslayer(IP):
-        ip_src = packet[IP].src
-        ip_dst = packet[IP].dst
-        port_src = packet[TCP].sport if packet.haslayer(TCP) else packet[UDP].sport
-        port_dst = packet[TCP].dport if packet.haslayer(TCP) else packet[UDP].dport
-        protocol = packet[IP].proto
-        protocol_name = {
-            1: 'ICMP',
-            6: 'TCP',
-            17: 'UDP',
-            47: 'GRE',
-            50: 'ESP',
-            51: 'AH',
-            89: 'OSPF',
-            132: 'SCTP',
-        }.get(protocol, 'Other')
+        if not packet.haslayer(TCP) and not packet.haslayer(UDP):
+            print(f"Non-TCP/UDP packet detected from {ip_src} to {ip_dst}")
+            
+        else:
+            ip_src = packet[IP].src
+            ip_dst = packet[IP].dst
+            port_src = packet[TCP].sport if packet.haslayer(TCP) else packet[UDP].sport
+            port_dst = packet[TCP].dport if packet.haslayer(TCP) else packet[UDP].dport
+            protocol = packet[IP].proto
+            protocol_name = {
+             1: 'ICMP',
+             6: 'TCP',
+                17: 'UDP',
+                47: 'GRE',
+                50: 'ESP',
+             51: 'AH',
+             89: 'OSPF',
+             132: 'SCTP',
+         }.get(protocol, 'Other')
 
         if protocol == 6:  # TCP protocol
             if port_dst == 22 or port_src == 22:
@@ -186,8 +191,10 @@ def packet_callback(packet):
         # Check for high connection rate outside business hours
         if not (BUSINESS_HOURS[0] <= current_time.hour <= BUSINESS_HOURS[1]):
             alerts.append(f"{Fore.YELLOW}ALERT: Connection outside business hours detected from {ip_src} {Style.RESET_ALL}")
+            print(alerts[-1])
         if hourly_connections[ip_src] > MAX_CONNECTIONS_PER_HOUR:
             alerts.append(f"{Fore.RED}ALERT: High connection rate detected from {ip_src} {Style.RESET_ALL}")
+            print(alerts[-1])
 
         # Detect scanning behavior (many connections to different ports)
         scan_activity[ip_src].append(current_time)
@@ -195,55 +202,62 @@ def packet_callback(packet):
             first_activity_time = scan_activity[ip_src][0]
             if (current_time - first_activity_time).total_seconds() < scan_detection_window:
                 alerts.append(f"ALERT: Possible scanning activity detected from {ip_src}")
+                print(alerts[-1])
 
 
         # Check if the header corresponds to the expected protocol
         if not check_header(packet, port_dst, protocol_name):
             alerts.append(f"{Fore.RED}ALERT: Packet header does not match expected protocol for flow {flow_id}{Style.RESET_ALL}")
+            print(alerts[-1])
 
         if ip_src.startswith(INTERNAL_IP_RANGE) and not ip_dst.startswith(INTERNAL_IP_RANGE): # Outbound flow
             if ip_dst in UNAUTHORIZED_IPS:
                 alerts.append(f"{Fore.RED}ALERT: Outbound flow to unauthorized IP {ip_dst} - Flow: {flow_id}{Style.RESET_ALL}")
+                print(alerts[-1])
 
             #if port_src not in AUTHORIZED_OUTBOUND_PORTS: not good, we need to check the destination port
              #   alerts.append(f"{Fore.RED}ALERT: Outbound flow from unauthorized port {port_src} - Flow: {flow_id}{Style.RESET_ALL}")
             if port_dst not in AUTHORIZED_OUTBOUND_PORTS:
                 alerts.append(f"{Fore.RED}ALERT: Outbound flow to unauthorized port {port_dst} - Flow: {flow_id}{Style.RESET_ALL}")
+                print(alerts[-1])
 
             if protocol not in AUTHORIZED_PROTOCOLS:
                 alerts.append(f"{Fore.RED}ALERT: Outbound flow using unauthorized protocol {protocol_name} - Flow: {flow_id}{Style.RESET_ALL}")
+                print(alerts[-1])
 
             if ip_dst not in AUTHORIZED_IPS:
                 alerts.append(f"{Fore.RED}ALERT: Outbound flow to {ip_dst} without prior DNS response - Flow: {flow_id}{Style.RESET_ALL}")
+                print(alerts[-1])
 
 
 
             if stats["packets"] >= DNS_PACKET_THRESHOLD and protocol_name == 'DNS':
                 alerts.append(f"{Fore.RED}ALERT: High number of DNS packets detected in outbound flow {flow_id} - Packet Count: {stats['packets']}{Style.RESET_ALL}")
+                print(alerts[-1])
 
             if  DNS_THRESHOLD > stats["bytes"] >= DNS_NORMAL_THRESHOLD and protocol_name == 'DNS':
                 alerts.append(f"{Fore.YELLOW}ALERT: DNS packet size above normal threshold in outbound flow {flow_id} - Byte Count: {stats['bytes']}{Style.RESET_ALL}")
-
+                print(alerts[-1])
             if stats["bytes"] >= DNS_THRESHOLD and protocol_name == 'DNS':
                 alerts.append(f"{Fore.RED}ALERT: High volume detected in outbound flow {flow_id} - Byte Count: {stats['bytes']}{Style.RESET_ALL}")
-
+                print(alerts[-1])
             if protocol_name == 'DNS' and packet[DNS].qd.qname in REQUESTED_DOMAINS:
                 alerts.append(f"{Fore.RED}ALERT: DNS exfiltration detected in outbound flow {flow_id} - Requested Domain: {packet[DNS].qd.qname}{Style.RESET_ALL}")
-            
+                print(alerts[-1])
             if protocol_name == 'DNS' and packet[DNS].qd.qname not in REQUESTED_DOMAINS:
                 REQUESTED_DOMAINS.add(packet[DNS].qd.qname)
-
+                print(alerts[-1])
             if protocol_name == 'DNS' and packet[DNS].qd.qname in UNAUTHORIZED_DOMAINS:
                 alerts.append(f"{Fore.RED}ALERT: Unauthorized domain requested in outbound flow {flow_id} - Requested Domain: {packet[DNS].qd.qname}{Style.RESET_ALL}")
-
+                print(alerts[-1])
                 
 
             if stats["bytes"] > VOLUME_THRESHOLD:
                 alerts.append(f"{Fore.RED}ALERT: High volume detected in outbound flow {flow_id} - Byte Count: {stats['bytes']}{Style.RESET_ALL}")
-
+                print(alerts[-1])
             if outbound_inernal_ip_usage[ip_src]["packets"] > AVG_PACKETS_THRESHOLD and outbound_inernal_ip_usage[ip_src]["bytes"] > AVG_THRESHOLD:
                 alerts.append(f"{Fore.RED}ALERT: High usage detected in internal IP {ip_src} - Byte Count: {outbound_inernal_ip_usage[ip_src]['bytes']}{Style.RESET_ALL}") 
-
+                print(alerts[-1])
 
 
 
@@ -254,34 +268,44 @@ def packet_callback(packet):
 
             if port_dst not in AUTHORIZED_INBOUND_PORTS:
                 alerts.append(f"{Fore.YELLOW}ALERT: Inbound flow to unauthorized port {port_dst} - Flow: {flow_id}{Style.RESET_ALL}")
+                print(alerts[-1])
             
 
 def start_sniffing(interface):
     sniff(iface=interface, prn=packet_callback, store=0, stop_filter=lambda x: stop_event.is_set())
 
-def print_flow_statistics():
+def generate_statistics(flow_statistics):
     stats_lines = []
     protocol_count = defaultdict(int)
     for flow, stats in flow_statistics.items():
-        line = f"Flow: {flow} - Packet Count: {stats['packets']} - Byte Count: {stats['bytes']} - Protocol: {stats['protocol']}"
+        line = {
+            "Flow": flow,
+            "Packet Count": stats['packets'],
+            "Byte Count": stats['bytes'],
+            "Protocol": stats['protocol']
+        }
         protocol_count[stats['protocol']] += stats['packets']
         stats_lines.append(line)
-    for protocol, count in protocol_count.items():
-        print(f"{Fore.GREEN}Protocol {protocol} - Number of Packets: {count}{Style.RESET_ALL}")
     return stats_lines
 
-def save_statistics_to_file(filename, stats_lines, alert_lines):
-    with open(filename, 'w') as file:
-        for line in stats_lines:
+def save_statistics_to_file(alertsFile,csvStatsFile, stats_lines, alert_lines):
+    df = pd.DataFrame(stats_lines)
+    df.to_csv(csvStatsFile, index=False)
+        
+    with open(alertsFile, 'w') as file:
+        for line in alert_lines:
             file.write(line + '\n')
-        file.write("\nAlerts:\n")
-        for alert in alert_lines:
-            file.write(alert + '\n')
+            
+
+
+    
+    
 
 def main():
     parser = argparse.ArgumentParser(description="Network Traffic Monitor")
     parser.add_argument('--internal-ip-prefix', required=True, help="The prefix of the internal IP range") 
-    parser.add_argument('--output-file', default="final_statistics.txt", help="The file to save statistics and alerts")
+    parser.add_argument('--Flow-stats-file', default="flow_statistics.csv", help="The file to save statistics per flow")
+    parser.add_argument('--alerts-file', default="alerts.txt", help="The file to save alerts")
     parser.add_argument('--interface', required= False, help="The network interface to sniff on") #For live capture
     parser.add_argument('--pcap-file', required=False, help="The pcap file to analyze") #For pcap file
     
@@ -291,7 +315,8 @@ def main():
     args = parser.parse_args()
     global INTERNAL_IP_RANGE
     INTERNAL_IP_RANGE = args.internal_ip_prefix
-    filename = args.output_file
+    stats_per_flow = args.Flow_stats_file   
+    alerts_file = args.alerts_file
 
     if args.interface is None and args.pcap_file is None:
         interfaces = get_if_list()
@@ -304,11 +329,12 @@ def main():
     if args.pcap_file: # Analyze pcap file
         print(f"Analyzing pcap file {args.pcap_file}...")
         sniff(offline=args.pcap_file, prn=packet_callback, store=0)
-        stats_lines = print_flow_statistics()
-        save_statistics_to_file(filename, stats_lines, alerts)
+        stats_lines = generate_statistics(flow_statistics)
+        save_statistics_to_file(alerts_file,stats_per_flow, stats_lines, alerts)
+        print(f"Final statistics per flow saved to {stats_per_flow} \nExiting...")
         for alert in alerts:
             print(alert)
-        print(f"Final statistics saved to {filename} \nExiting...")
+        print(f"Alerts saved to {alerts_file} \nExiting...")
         return
     
     
@@ -319,19 +345,19 @@ def main():
 
     try:
         while not stop_event.is_set():
-            time.sleep(10)  # Every 10 seconds, print the statistics and check for alerts
-            print_flow_statistics()
-            for alert in alerts:
-                print(alert)
-            alerts.clear()
+            # Keep the main thread alive
+            time.sleep(1)
     except KeyboardInterrupt:
         print("KeyboardInterrupt received, stopping capture...")
     finally:
         stop_event.set()
         sniff_thread.join()
-        stats_lines = print_flow_statistics()
-        save_statistics_to_file(filename, stats_lines, alerts)
-        print(f"Final statistics saved to {filename} \nExiting...")
+        stats_lines = generate_statistics(flow_statistics)
+        save_statistics_to_file(alerts_file,stats_per_flow, stats_lines, alerts)
+        print(f"Final statistics per flow saved to {stats_per_flow} \nExiting...")
+        print(f"Alerts saved to {alerts_file} \nExiting...")
+        return
 
 if __name__ == "__main__":
     main()
+
